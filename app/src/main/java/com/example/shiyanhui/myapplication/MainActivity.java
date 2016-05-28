b55e5ab370f1b151ad7d085c629a06fa8729c624
eccentricyan <eccentricyan@gmail.com>
2016-05-28 14:15:11 +0900

    fix

--- app/src/main/java/com/example/shiyanhui/myapplication/MainActivity.java
+++ app/src/main/java/com/example/shiyanhui/myapplication/MainActivity.java
@@ -1,25 +1,24 @@
 package com.example.shiyanhui.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Timer mTimer;
     private float mCounter;
-    private TextView mCounterText;
+
+    @InjectView(R.id.counterText)
+    TextView mCounterText;
     private Handler mHandler = new Handler();
     private Boolean isStarted = false;
 
@@ -27,74 +26,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        Button start = (Button)findViewById(R.id.start);
-        Button stop = (Button)findViewById(R.id.stop);
-        Button reset = (Button)findViewById(R.id.reset);
-        mCounterText = (TextView)findViewById(R.id.counterText);
-
-        start.setOnClickListener(this);
-        stop.setOnClickListener(this);
-        reset.setOnClickListener(this);
-
-        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+        ButterKnife.inject(this);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     class MyTimerTask extends TimerTask {
         @Override
@@ -138,6 +72,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 mCounter = 0;
                 stopTimer();
                 mCounterText.setText("0:00.000");
+                break;
+
+            case R.id.change:
+                Intent intent = new Intent(this, Main2Activity.class);
+                startActivity(intent);
                 break;
         }
     }
